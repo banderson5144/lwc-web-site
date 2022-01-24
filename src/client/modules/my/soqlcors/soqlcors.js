a0454fbadf9692779bc1710fd9298d79c266447a
Bryan Anderson <banderson5144@gmail.com>
2022-01-24 13:25:45 -0500

    Update, see what happens

---
+++ src/client/modules/my/soqlcors/soqlcors.js
@@ -0,0 +1,63 @@
+import { LightningElement } from 'lwc';
+
+export default class BasicDatatable extends LightningElement {
+    data;
+    columns;
+    soqlStr;
+    isSet = false;
+
+    connectedCallback()
+    {
+        let myServ = this.getCookie('myServ');
+        let mySess = this.getCookie('mySess');
+
+        this.isSet = (myServ !== null && mySess !== null);
+    }
+
+    handleChange(event) {
+        this.soqlStr = event.target.value;
+    }
+
+    getCookie(name) {
+        var cookieString = '; ' + document.cookie;
+        var parts = cookieString.split('; ' + name + '=');
+        if (parts.length === 2) {
+            return parts.pop().split(';').shift();
+        }
+        return null;
+    }
+
+    callQuery() {
+        const myHeaders = new Headers();
+        myHeaders.append('Authorization', 'Bearer ' + this.getCookie('mySess'));
+
+        const sfServer =
+            decodeURIComponent(this.getCookie('myServ')) +
+            '/services/data/v52.0/query?q=' +
+            encodeURIComponent(this.soqlStr);
+
+        const myRequest = new Request(sfServer, {
+            method: 'GET',
+            headers: myHeaders,
+            mode: 'cors',
+            cache: 'default'
+        });
+
+        fetch(myRequest)
+        .then((response) => response.json())
+        .then((myJson) => {
+            console.log(JSON.stringify(myJson));
+            this.data = myJson.records;
+
+            let newCols = [];
+
+            for (const fld of Object.keys(myJson.records[0])) {
+                if (fld !== 'attributes') {
+                    newCols.push({ label: fld, fieldName: fld });
+                }
+            }
+
+            this.columns = newCols;
+        });
+    }
+}
---
+++ src/client/modules/my/testcoverage/testcoverage.js
@@ -0,0 +1,91 @@
+import { LightningElement, track } from 'lwc';
+import j2c from 'json2csv';
+
+export default class App extends LightningElement {
+    success = false;
+    isSet = false;
+    canDownload = false;
+    orgCoverage=0;
+    columns = [
+        {
+            label: 'Apex Class or Trigger name',
+            fieldName: 'name'
+        },
+        {
+            label: 'Code Coverage Percentage',
+            fieldName: 'coverage',
+            cellAttributes: {
+                class: {
+                    fieldName: 'format'
+                }
+            }            
+        }
+    ];
+    @track tblData = [{ name: 'foo', coverage: '1%' }];
+
+    connectedCallback() {
+        var searchParams = new URLSearchParams(window.location.search);
+
+        if (searchParams != null && searchParams.has('success')) {
+            this.isSet = true;
+            this.success = searchParams.get('success') === 'true';
+        }
+    }
+
+    handleClick(evt) {
+        fetch('/getcoverage')
+            .then((response) => response.json())
+            .then((data) => {
+                var covTbl = {};
+                var newTblData = [];
+                for(const rec of data.codeCov.records)
+                {
+                    if(!covTbl.hasOwnProperty(rec.ApexClassOrTrigger.Name))
+                    {
+                        covTbl[rec.ApexClassOrTrigger.Name] = {
+                            'numLinesCovered':rec.NumLinesCovered,
+                            'numLinesUncovered':rec.NumLinesUncovered
+                        };
+                    }else
+                    {
+                        covTbl[rec.ApexClassOrTrigger.Name].numLinesCovered+=rec.NumLinesCovered;
+                        covTbl[rec.ApexClassOrTrigger.Name].numLinesUncovered+=rec.NumLinesUncovered;
+                    }
+                }
+
+                for(const p in covTbl)
+                {
+                    var numLinesCovered = covTbl[p].numLinesCovered;
+                    var numLinesUncovered = covTbl[p].numLinesUncovered;
+
+                    if(numLinesCovered == 0 && numLinesUncovered == 0)
+                    {
+                        newTblData.push({name:p,coverage:'0%',format:'slds-text-color_error'});
+                    }else
+                    {
+                        var cov = parseInt(numLinesCovered/(numLinesCovered+numLinesUncovered)*100);
+                        newTblData.push({name:p,coverage:cov+'%',format:(cov>=75?'slds-text-color_success':'slds-text-color_error')});
+                    }
+                }
+
+                this.orgCoverage = data.orgCov.records[0].PercentCovered;
+                this.tblData = newTblData;
+            });
+    }
+
+    downloadCsv(evt) {
+
+        var myData = this.tblData;
+
+        const fields = ['name', 'count'];
+        const opts = { fields };
+
+        const csv = j2c.parse(myData, opts);
+
+        var hiddenElement = document.createElement('a');
+        hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+        hiddenElement.target = '_blank';
+        hiddenElement.download = 'sf_record_count.csv';
+        hiddenElement.click();
+    }
+}
