70c9e814ef28da6cb724531cb46c0553267e35da
Bryan Anderson <banderson5144@gmail.com>
2022-02-03 17:57:53 -0500

    Guess this was it

--- scripts/server.js
+++ scripts/server.js
@@ -23,10 +23,10 @@ app.use(cookieParser());
 app.use(helmet());
 app.use(compression());
 app.use(cors());
-// app.use(function(req, res, next) {
-//     req.headers['if-none-match'] = 'no-match-for-this';
-//     next();    
-//   });
+app.use(function(req, res, next) {
+    req.headers['if-none-match'] = 'no-match-for-this';
+    next();    
+  });
 app.use(function (req, res, next) {
     console.log('Time:', Date.now());
     res.set('Content-Security-Policy', 'connect-src '+req.cookies.myServ);
