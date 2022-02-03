b7cc672a140aea9fa67cbf7953abb57f1c1cac5d
Bryan Anderson <banderson5144@gmail.com>
2022-02-03 16:54:00 -0500

    Another test

--- scripts/server.js
+++ scripts/server.js
@@ -24,21 +24,6 @@ app.use(helmet());
 app.use(compression());
 app.use(cors());
 
-// app.use(function (req, res, next)
-// {
-//     let reqId = req.headers['x-request-id'];
-//     console.log('csp middleware begin-'+reqId);
-//     if (req.url === '/' || req.url === '/?success=true')
-//     {
-//         console.log('Should set CSP-'+reqId);
-//         res.set("Content-Security-Policy", "connect-src 'self' "+req.cookies.myServ);
-//         console.log('after setting CSP-'+reqId);
-//     }
-//     console.log('csp middleware right before next-'+reqId);
-//     next();
-//     console.log('csp middleware right after next-'+reqId);
-// });
-
 app.use(express.static(DIST_DIR,{
     etag: false
 }));
@@ -143,6 +128,7 @@ app.get('/getcounts',function(req,res)
 
 app.use((req, res) =>
 {
+    console.log('Am I getting here');
     console.log(req.cookies.myServ);
     res.set("Content-Security-Policy", "connect-src 'self' "+req.cookies.myServ);
     res.sendFile(path.resolve(DIST_DIR, 'index.html'));
