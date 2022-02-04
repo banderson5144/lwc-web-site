58188941c2ac98f6d6ceffc0188bebaf0b7efac5
Bryan Anderson <banderson5144@gmail.com>
2022-02-04 18:36:03 -0500

    Revert back

--- scripts/server.js
+++ scripts/server.js
@@ -72,7 +72,7 @@ app.get('/oauth2/callback', function(req, res)
             console.log(conn.instanceUrl);
             res.cookie('mySess',conn.accessToken, {maxAge:900000});
             res.cookie('myServ',conn.instanceUrl, {maxAge:900000});
-            res.end();
+            res.redirect('/?success=true');
         })
         .catch(err =>{
             console.log(err);
